aeea42f22276356c68af2e507d0c5b5411dddd23
guyixiang <2190958099@qq.com>
2020-11-13 23:55:56 +0800

    1234567

---
+++ 红岩第四次作业.js
@@ -0,0 +1,45 @@
+// L0
+const handsomeboy = {
+    name: 'NCT',
+    membernumber:23,
+    introduce: {
+    advantage: 'have a funny soul',
+    disadvantage: 'so pretty'
+    }
+}
+    let [a,b]=[1,2];
+    let [c]=[1,2];
+    let [d,,e]=[1,2,3];
+    let [f,g=4]=[1];
+    let {h,i}={h:"5",i:"6"};
+    let {j}={h:"5",i:"6"};
+    let { nct : nctzen }={ nct: "23"};
+//L1
+let num;
+function f(num)
+if(num<2)
+{
+    return num;
+}
+else
+{
+    let f1,f2,i;
+    f1=i
+    for(i=2;i<num;i++)
+    {
+        num=f1+f2;
+        f1=f2;
+        f2=num;
+    }
+    return num;
+}
+//L2
+let arr=[1,2,3,4,5,6];
+function getSum(){
+    let sum=0
+    arr=[]
+    sum+=arr
+    console.log(sum)
+}
+getSum()
+//L3
